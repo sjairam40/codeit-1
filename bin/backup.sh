e0a130d0209a5b5fb1074a9e36f55e5b2747c481
Sanjay Jairam <sanjay_jairam@harvard.edu>
2024-05-27 08:24:51 -0600

    backup script

---
+++ bin/backup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+echo " --> Going to USB"
+cd /Volumes/Data2024/
+
+month=`date +%b`
+echo " --> Creating Month docco"
+
+DOCCO='Documents-$month-2024'
+
+if [ ! -d ${DOCCO} ]; 
+then
+    mkdir Documents-$month-2024
+else 
+    echo " --> Folder exists"
+fi
+
+echo " --> Going to Docs"
+cd ~/Documents
+sleep 5
+
+echo " --> Copying 00-docs"
+cp -R 00-docs-00 /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copying 00-personal"
+cp -R 00-personal-00 /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copying 4-DEV"
+cp -R 4-DEV* /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copying 5-QA"
+cp -R 5-QA* /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copying ARGO"
+cp -R ARGO* /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copying 0 txts"
+cp -R 0*.txt /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copy D&G"
+cp -R Domestic* /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+# echo " --> Copy GitHub"
+# cp -R GitHub* /Volumes/Data2024/Documents-$month-2024/
+# sleep 5
+
+echo " --> Copy Harvard"
+cp -R HARVARD* /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copy ISTIO"
+cp -R ISTIO* /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copy JURICA"
+cp -R JURICA* /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copy Orca"
+cp -R ORCA* /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copy RKE"
+cp -R RKE* /Volumes/Data2024/Documents-$month-2024/
+sleep 5
+
+echo " --> Copy Zoom"
+cp -R Zoom* /Volumes/Data2024/Documents-$month-2024/
+
+echo " --> Copy zz-home"
+cp -R zz-home* /Volumes/Data2024/Documents-$month-2024/
+
+echo " --> Copy Installed"
+cp -R zz-installed* /Volumes/Data2024/Documents-$month-2024/
