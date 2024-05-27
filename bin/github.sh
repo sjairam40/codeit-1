376b54b64a437c3651aae1e0a905375a5ebde7fc
Sanjay Jairam <sanjay_jairam@harvard.edu>
2024-05-27 08:24:37 -0600

    github checkout script

---
+++ bin/github.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cd ~/Documents/GitHub/
+mkdir APPS-INFRA
+cd APPS-INFRA
+
+echo " --> A to L"
+
+git clone git@github.huit.harvard.edu:LTS/ACORN-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/archecker-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/argoCD-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/aspace-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/booklabeler-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/curiosity-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/DAIS-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/DRS-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/fts-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/HDForms-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/hgl-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/IDS-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/imgconv-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/jobmonitor-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/JSTOR-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/LISTVIEW-INFRA.git
+
+git clone git@github.huit.harvard.edu:LTS/lts-pipelines-INFRA.git
