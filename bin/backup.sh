55a1dd7cb57f1f4bd711b91bd35112912752c463
Sanjay Jairam <sanjay_jairam@harvard.edu>
2024-06-08 16:16:52 -0600

    Simplify A code

--- bin/backup.sh
+++ bin/backup.sh
@@ -35,12 +35,8 @@ echo " --> Copying 5-QA"
 cp -R 5-QA* /Volumes/Data2024/Documents-$month-2024/
 sleep 5
 
-echo " --> Copying AMBIENT"
-cp -R AMB* /Volumes/Data2024/Documents-$month-2024/
-sleep 5
-
-echo " --> Copying ARGO"
-cp -R ARGO* /Volumes/Data2024/Documents-$month-2024/
+echo " --> Copying A*"
+cp -R A* /Volumes/Data2024/Documents-$month-2024/
 sleep 5
 
 echo " --> Copying 0 txts"
