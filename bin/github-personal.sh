851ce3625b548395803bb59227e1fe354725712d
Sanjay Jairam <sanjay_jairam@harvard.edu>
2024-06-25 11:31:41 -0600

    github personal repos

---
+++ bin/github-personal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~/Documents/GitHub/
+mkdir PERS-tmp
+cd PERS-tmp
+
+echo "  --> aws docker "
+git clone git@github.com:sjairam/aws-docker-githubActons-kubernetes-demo.git 
+
+echo " --> ci-cd intro"
+git clone https://github.com/Link-/ci-cd-intro
+
+echo " --> codeit "
+git clone git@github.com:sjairam/codeit.git
+
