3a684e3c8454c7f9fa9371c7dcaac8705e4fa551
Sanjay Jairam <sanjay_jairam@harvard.edu>
2024-07-21 10:47:30 -0600

    Add another repo - k8s

--- bin/github-personal.sh
+++ bin/github-personal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd ~/Documents/GitHub/
-mkdir PERS-tmp
-cd PERS-tmp
+mkdir PERS
+cd PERS
 
 echo "  --> aws docker "
 git clone git@github.com:sjairam/aws-docker-githubActons-kubernetes-demo.git 
@@ -12,4 +12,7 @@ git clone https://github.com/Link-/ci-cd-intro
 
 echo " --> codeit "
 git clone git@github.com:sjairam/codeit.git
+
+echo "k8s coursee"
+git clone https://gitlab.com/twn-cka-course/latest/k8s-administrator-course
 
