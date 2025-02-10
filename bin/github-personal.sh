8cb3fb9d714f3ee5311ff80b366c10ecc9a23c4b
sjairam <sjairam@oteemo.com>
2025-02-10 14:46:40 -0600

    Add github-personal script

--- bin/github-personal.sh
+++ bin/github-personal.sh
@@ -13,6 +13,11 @@ git clone https://github.com/Link-/ci-cd-intro
 echo " --> codeit "
 git clone git@github.com:sjairam/codeit.git
 
-echo "k8s coursee"
+echo " --> demo "
+git clone git@github.com:sjairam/demo-container-gh-actions.git
+
+echo " --> k8s coursee"
 git clone https://gitlab.com/twn-cka-course/latest/k8s-administrator-course
 
+echo " --> terraform github actions"
+git clone git@github.com:sjairam/terraform-github-actions-aws.git
