35e594898c40c1b9d19b46abcc211a6f1f0127a4
sjairam <sjairam@oteemo.com>
2025-02-12 10:34:20 -0600

    Update github-tools

--- bin/github-tools.sh
+++ bin/github-tools.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+start_time=$(date +%s)
+
+month=`date +%b`
+
+year=`date +%y`
+
+
 cd ~/Documents/GitHub/
 mkdir TOOLS
 cd TOOLS
@@ -9,6 +16,9 @@ git clone git@github.com:prometheus/alertmanager.git
 
 echo " --> argocd"
 git clone git@github.com:argoproj/argo-cd.git
+
+echo " --> aws ebs driver"
+git clone git@github.com:kubernetes-sigs/aws-ebs-csi-driver.git
 
 echo " --> csi nfs driver"
 git clone git@github.com:kubernetes-csi/csi-driver-nfs.git
@@ -24,3 +34,16 @@ git clone git@github.com:derailed/popeye.git
 
 echo " --> samber/prometheus"
 git clone git@github.com:samber/awesome-prometheus-alerts.git
+
+
+end_time=$(date +%s)
+
+elapsed_time=$(( end_time - start_time ))
+
+# Convert elapsed time to minutes and seconds
+elapsed_minutes=$(( elapsed_time / 60 ))
+elapsed_seconds=$(( elapsed_time % 60 ))
+
+echo " ====================================================== "
+# Output the result
+echo "Elapsed time: $elapsed_minutes minutes and $elapsed_seconds seconds"
