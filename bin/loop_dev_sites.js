cbc91d68f745517732c73423bb15a8d3447ffab2
sjairam <sjairam@oteemo.com>
2025-02-18 15:42:05 -0600

    Update Aspace DEV

--- bin/loop_dev_sites.js
+++ bin/loop_dev_sites.js
@@ -2,13 +2,14 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-    vus: 4,           // Number of virtual users
+    vus: 1,           // Number of virtual users
     duration: '10s',  // Duration of the test
 };
 
 const urls = [ 
     'https://acorn-dev.lib.harvard.edu/records/list',
     'https://arclight-dev.lib.harvard.edu/',
+    'https://aspace-dev.lib.harvard.edu',
     'https://booklabeler-dev.lib.harvard.edu',
     'https://curiosity-dev.lib.harvard.edu',
     'https://cluster-console.dev.lib.harvard.edu',
