84d74ebbbc6516e84701fc52a05dddfe59ca81b2
sjairam <sjairam@oteemo.com>
2025-02-18 13:53:59 -0600

    Update dev sites

---
+++ bin/loop_dev_sites.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    vus: 4,           // Number of virtual users
+    duration: '10s',  // Duration of the test
+};
+
+const urls = [ 
+    'https://acorn-dev.lib.harvard.edu/records/list',
+    'https://arclight-dev.lib.harvard.edu/',
+    'https://booklabeler-dev.lib.harvard.edu',
+    'https://curiosity-dev.lib.harvard.edu',
+    'https://cluster-console.dev.lib.harvard.edu',
+    // 'https://another-site.com',
+];
+
+export default function () {
+    for (let url of urls) {
+        let response = http.get(url);
+        console.log(`Response time for ${url}: ${response.timings.duration} ms`);
+        sleep(1);  // Wait for 1 second between requests
+    }
+}
