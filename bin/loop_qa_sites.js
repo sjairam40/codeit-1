3c5b4a9a5161afabfe912ab3b54f3fb845933868
sjairam <sjairam@oteemo.com>
2025-02-18 13:53:37 -0600

    Update qa sites

--- bin/loop_qa_sites.js
+++ bin/loop_qa_sites.js
@@ -2,21 +2,24 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-    vus: 4,           // Number of virtual users
+    vus: 1,           // Number of virtual users
     duration: '10s',  // Duration of the test
 };
 
 const urls = [ 
     'https://acorn-qa.lib.harvard.edu/records/list',
     'https://arclight-qa.lib.harvard.edu/',
+    'https://aspace-qa.lib.harvard.edu',
+    'https://bibdata-qa.lib.harvard.edu',
     'https://booklabeler-dev.lib.harvard.edu',
     'https://curiosity-qa.lib.harvard.edu',
     'https://dims-qa.lib.harvard.edu',
     'https://eda-qa.lib.harvard.edu',
+    'https://embed-qa.lib.harvard.edu',
     'https://fts-qa.lib.harvard.edu',
     'https://geodata-qa.lib.harvard.edu',
     'https://hgl-qa.lib.harvard.edu',
-    'https://cluster-console.dev.lib.harvard.edu',
+    'https://cluster-console.qa.lib.harvard.edu',
     // 'https://another-site.com',
 ];
 
