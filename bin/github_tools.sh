91296e76e222e61d2cad6bf99ba5b34ece1e1242
sjairam <sjairam@oteemo.com>
2025-03-13 20:34:19 +0000

    Add istio.io github-tools

--- bin/github_tools.sh
+++ bin/github_tools.sh
@@ -6,6 +6,11 @@ month=`date +%b`
 
 year=`date +%y`
 
+clear
+
+timeSec=2
+
+echo " ==>> Creating repos "
 
 cd ~/Documents/GitHub/
 mkdir TOOLS
@@ -13,37 +18,51 @@ cd TOOLS
 
 echo "  --> alertmanager "
 git clone git@github.com:prometheus/alertmanager.git
+sleep $timeSec
 
 echo " --> argocd"
 git clone git@github.com:argoproj/argo-cd.git
+sleep $timeSec
 
 echo " --> aws ebs driver"
 git clone git@github.com:kubernetes-sigs/aws-ebs-csi-driver.git
+sleep $timeSec
 
 echo " --> csi nfs driver"
 git clone git@github.com:kubernetes-csi/csi-driver-nfs.git
+sleep $timeSec
 
 echo " --> helm "
 git clone git@github.com:helm/helm.git
+sleep $timeSec
 
 echo " --> iamlive"
 git clone git@github.com:iann0036/iamlive.git
+sleep $timeSec
 
 echo " --> ISTIO" 
 git clone git@github.com:istio/istio.git
+sleep $timeSec
+
+echo " --> istio.io"
+git clone git@github.com:istio/istio.io.git
+sleep $timeSec
 
 echo " --> jaeger"
 git clone git@github.com:jaegertracing/jaeger.git
+sleep $timeSec
 
 echo " --> Krew plugins"
 git clone git@github.com:kubernetes-sigs/krew.git
+sleep $timeSec
 
 echo " --> popeye"
 git clone git@github.com:derailed/popeye.git
+sleep $timeSec
 
 echo " --> samber/prometheus"
 git clone git@github.com:samber/awesome-prometheus-alerts.git
-
+sleep $timeSec
 
 end_time=$(date +%s)
 
